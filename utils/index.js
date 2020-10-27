43a6c25b4a499eb9c18b1ce871f60bfa8aadf7aa
sanSkiller <1290148953@qq.com>
2020-10-27 22:22:44 +0800

    updated: 校验验证码工具方法

---
+++ utils/index.js
@@ -0,0 +1,19 @@
+/**
+ * 通用的工具方法
+ */
+const { getRedisVal } = require("../utils/redis");
+
+/**
+ * 核对验证码
+ * @param {String} sid sid值
+ * @param {String} text 客户端发来的验证文本
+ */
+const checkCaptcha = async (sid, text) => {
+  let result = await getRedisVal(sid);
+
+  return result === text;
+};
+
+module.exports = {
+  checkCaptcha,
+};
