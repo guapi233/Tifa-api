dd5c2706d55ad17b794ac867893909bb3087c68b
sanSkiller <1290148953@qq.com>
2020-10-27 09:06:45 +0800

    updated: 集成mongoose

---
+++ utils/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const config = require("../config/index");
+
+// 连接 MongoDB
+mongoose.connect(config.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// 监听
+mongoose.connection.once("connected", () => {
+  console.log("Mongoose connection open to " + config.DB_URL);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose connection appear error: " + err);
+});
+
+mongoose.connection.once("disconnected", () => {
+  console.log("Mongoose connection disconected");
+});
+
+module.exports = mongoose;
