6d451639fc2934ffbd8205d756e3f1d4cb84c35d
sanSkiller <1290148953@qq.com>
2020-10-27 11:28:32 +0800

    updated: 添加注释

--- config/index.js
+++ config/index.js
@@ -1,12 +1,16 @@
+// MongoDB地址
 const DB_URL = "mongodb://localhost/Tifa";
 
+// JWT密钥
 const JWT_SECRET = "hi man, I think you are a gay!";
 
+// Redis配置
 const REDIS = {
   host: "localhost",
   port: 6379,
 };
 
+// 验证码有效期
 const CAPTCHA_LIFE = 10 * 60;
 
 module.exports = {
