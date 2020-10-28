9091da2ae77143014fd96bbe8689e1d9c501f3ab
sanSkiller <1290148953@qq.com>
2020-10-28 22:22:26 +0800

    建立文章数据模型

---
+++ model/Article.js
@@ -0,0 +1,44 @@
+const mongoose = require("../utils/db");
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+  author: Object,
+  banner: {
+    type: String,
+    default: "/img/pic.png",
+  },
+  commentCount: {
+    type: Number,
+    default: 0,
+  },
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  viewCount: {
+    type: Number,
+    default: 0,
+  },
+  created: {
+    type: Date,
+    default: Date.now(),
+  },
+  modified: {
+    type: Date,
+    default: Date.now(),
+  },
+  title: String,
+  id: {
+    type: Number,
+    index: {
+      unique: true,
+    },
+  },
+});
+
+const ArticleModel = mongoose.model("articles", ArticleSchema);
+
+module.exports = {
+  ArticleModel,
+};
