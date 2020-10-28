29ff2b0f6608af06e6fc26d99c792cb4fd353f27
sanSkiller <1290148953@qq.com>
2020-10-28 17:34:54 +0800

    updated: 验证用户是否存在

--- routes/public.js
+++ routes/public.js
@@ -1,8 +1,20 @@
 const router = require("koa-router")();
 const publicController = require("../controller/publicController");
+const { userIsExist } = require("../model/User");
 
 router.prefix("/public");
 
 router.get("/getCaptcha", publicController.getCaptcha);
+
+router.get("/exist", async (ctx) => {
+  let { usernumber } = ctx.query;
+
+  let result = await userIsExist({ usernumber });
+
+  ctx.body = {
+    isOk: Number(result),
+    data: result,
+  };
+});
 
 module.exports = router;
