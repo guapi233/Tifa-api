a712e2f082938e96e7393d9628d8e9e77ca9888a
sanSkiller <1290148953@qq.com>
2020-11-08 16:08:20 +0800

    收藏数据模型

---
+++ model/Collection.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CollectionSchema = new Schema({
+  collectionId: {
+    type: String,
+    index: {
+      unique: true,
+    },
+  },
+  targetId: String,
+  authorId: String,
+  created: Date,
+});
+
+const CollectionModel = mongoose.model("collections", CollectionSchema);
+
+// 新增收藏
+const newCollection = async (collectionObj) => {
+  const { targetId, authorId } = collectionObj;
+
+  let newer = new CollectionModel({
+    targetId,
+    authorId,
+  });
+
+  let res = await newer.save();
+
+  if (!res) {
+    return false;
+  }
+  return newer.toObject();
+};
+
+// 是否已经收藏
+const isCollected = async (targetId, authorId) => {
+  let res = await CollectionModel.findOne({ targetId, authorId }, "_id");
+
+  if (!res) {
+    return false;
+  }
+  return true;
+};
+
+// 删除一条收藏记录（取消收藏）
+const delCollection = async (targetId) => {
+  let res = await CollectionModel.deleteOne({ targetId });
+
+  if (!res) {
+    return false;
+  }
+  return true;
+};
+
+module.exports = {
+  CollectionModel,
+  newCollection,
+  isCollected,
+  delCollection,
+};
