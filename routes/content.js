bc57680487480d881f322aed0499f74e7ac9de75
sanSkiller <1290148953@qq.com>
2020-11-11 11:06:18 +0800

    updated: 保存草稿 & 删除草稿 & 获取草稿列表|详情 & 添加前置钩子

--- routes/content.js
+++ routes/content.js
@@ -1,7 +1,17 @@
 const router = require("koa-router")();
 const contentController = require("../controller/contentController");
+const { getJwtPaload } = require("../utils/index");
 
 router.prefix("/content");
+
+// 前置钩子
+router.use("/", async (ctx, next) => {
+  // 解析token 获取usernumber
+  const { usernumber } = getJwtPaload(ctx.header["authorization"]);
+  ctx.usernumber = usernumber;
+
+  await next();
+});
 
 // 添加评论
 router.post("/addComment", contentController.addComment);
@@ -11,5 +21,17 @@ router.get("/addLike", contentController.addLike);
 
 // 添加收藏
 router.get("/addCollection", contentController.addCollection);
+
+// 保存草稿
+router.post("/saveDraft", contentController.saveDraft);
+
+// 删除草稿
+router.get("/delDraft", contentController.delDraft);
+
+// 获取草稿列表
+router.get("/getDraftList", contentController.getDraftList);
+
+// 获取草稿详情
+router.get("/getDraftDetail", contentController.getDraftDetail);
 
 module.exports = router;
