42f06842c80b7e5b111bdc89ff8b8a7c5d779eab
sanSkiller <1290148953@qq.com>
2020-11-23 20:08:25 +0800

    deleted: 移除无用注释

--- utils/index.js
+++ utils/index.js
@@ -43,7 +43,6 @@ const getJwtPaload = (token) => {
 
   // 判断是否过期（这里的判断没有必要，因为过期的token已经被 koa-jwt 拦截下了）
   // if (payload.exp * 1000 < Date.now()) return false;
-  console.log(payload);
   return payload;
 };
 
