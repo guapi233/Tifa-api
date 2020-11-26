f2cc383fd7452fd3f86c748c9b5b5b1e2ad0fb35
sanSkiller <1290148953@qq.com>
2020-11-26 16:44:20 +0800

    私信消息模型

---
+++ model/whisper.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { getUuid } = require("../utils/index");
+
+const Schema = mongoose.Schema;
+
+const WhisperSchema = new Schema({
+  whisperId: {
+    type: String,
+    index: {
+      unique: true,
+    },
+  },
+  roomId: String,
+  authorId: String,
+  show: Array,
+  content: String,
+  created: Date,
+  type: Number,
+  status: {
+    type: Number,
+    default: 1,
+  },
+});
+
+const WhisperModel = mongoose.model("whispers", WhisperSchema);
+
+/**
+ * 添加一条私信
+ * @param {Object} whisperObj 私信信息对象
+ */
+const newWhisper = async (whisperObj) => {
+  let newer = new WhisperModel({
+    ...whisperObj,
+    whisperId: getUuid(),
+    created: Date.now(),
+  });
+
+  try {
+    await newer.save();
+
+    return newer.toObject();
+  } catch (err) {
+    return false;
+  }
+};
+
+module.exports = {
+  WhisperModel,
+  newWhisper,
+};
