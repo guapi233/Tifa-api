08540cfe1c3abede5c8556e6ef4c3ad9ffefe9ef
sanSkiller <1290148953@qq.com>
2020-11-27 17:59:34 +0800

    modified: whisper => Whisper

---
+++ model/Whisper.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const { getUuid } = require("../utils/index");
+const { setUpdated } = require("./Room");
+
+const Schema = mongoose.Schema;
+
+const WhisperSchema = new Schema({
+  whisperId: {
+    type: String,
+    index: {
+      unique: true,
+    },
+  },
+  roomId: String,
+  targetId: String,
+  authorId: String,
+  isRead: {
+    type: Number,
+    default: 1,
+  },
+  hidden: {
+    type: Array,
+    default: [],
+  },
+  content: String,
+  created: Date,
+  type: Number,
+  status: {
+    type: Number,
+    default: 1,
+  },
+});
+
+const WhisperModel = mongoose.model("whispers", WhisperSchema);
+
+/**
+ * 添加一条私信
+ * @param {Object} whisperObj 私信信息对象
+ */
+const newWhisper = async (whisperObj) => {
+  let newer = new WhisperModel({
+    ...whisperObj,
+    whisperId: getUuid(),
+    created: Date.now(),
+  });
+
+  try {
+    await newer.save();
+    setUpdated(whisperObj.roomId);
+
+    return newer.toObject();
+  } catch (err) {
+    return false;
+  }
+};
+
+const getUnReaders = async (belongId) => {
+  let res = await WhisperModel.find({});
+};
+
+module.exports = {
+  WhisperModel,
+  newWhisper,
+  getUnReaders,
+};
